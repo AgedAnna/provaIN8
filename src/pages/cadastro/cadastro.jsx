726469a28cb52ecca0f06f73a5342194229127e7
Agedanna <annabeatryz12345@gmail.com>
2024-02-18 11:03:04 -0300

    feat: finalizando

--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -4,6 +4,8 @@ import { initializeApp } from "firebase/app";
 import { addDoc, deleteDoc, doc } from "firebase/firestore";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 import Lista from "../lista/lista";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Cadastro = () => {
   const firebaseApp = initializeApp({
@@ -16,25 +18,77 @@ const Cadastro = () => {
   const [email, setEmail] = useState("");
   const [telefone, setTelefone] = useState("");
   const [nascimento, setNascimento] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [errors, setErrors] = useState({});
 
   const [users, setUsers] = useState([]);
 
   const db = getFirestore(firebaseApp);
   const userCollection = collection(db, "users");
 
+  useEffect(() => {
+    const validateForm = () => {
+      const newErrors = {};
+      let isValid = true;
+
+      if (!name.trim()) {
+        newErrors.name = "Nome é obrigatório.";
+        isValid = false;
+      }
+
+      if (!email.trim() || !/\S+@\S+\.\S+/.test(email)) {
+        newErrors.email = "E-mail inválido.";
+        isValid = false;
+      }
+
+      if (!telefone.trim() || telefone.length < 8 || telefone.length > 13) {
+        newErrors.telefone = "Telefone inválido.";
+        isValid = false;
+      }
+
+      if (!nascimento.trim()) {
+        newErrors.nascimento = "Data de nascimento é obrigatória.";
+        isValid = false;
+      }
+
+      setErrors(newErrors);
+      setIsButtonDisabled(!isValid);
+    };
+
+    validateForm();
+  }, [name, email, telefone, nascimento]);
+
   async function createUser() {
-    const user = await addDoc(userCollection, {
-      name,
-      email,
-      nascimento,
-      telefone,
-    });
-    console.log(user);
+    try {
+      const user = await addDoc(userCollection, {
+        name,
+        email,
+        nascimento,
+        telefone,
+      });
+      toast.success("Usuário criado com sucesso!");
+      console.log(user);
+      setUsers([...users, { name, email, nascimento, telefone, id: user.id }]);
+      setName("");
+      setEmail("");
+      setTelefone("");
+      setNascimento("");
+    } catch (error) {
+      toast.error("Erro ao criar usuário.");
+      console.error("Error creating user: ", error);
+    }
   }
 
   async function deleteUser(id) {
-    const userDoc = doc(db, "users", id);
-    await deleteDoc(userDoc);
+    try {
+      const userDoc = doc(db, "users", id);
+      await deleteDoc(userDoc);
+      toast.success("Usuário excluído com sucesso!");
+      setUsers(users.filter((user) => user.id !== id));
+    } catch (error) {
+      toast.error("Erro ao excluir usuário.");
+      console.error("Error deleting user: ", error);
+    }
   }
 
   useEffect(() => {
@@ -57,7 +111,9 @@ const Cadastro = () => {
         <h1 className="titleCad">Cadastro</h1>
         <div className="input-container">
           <div className="input-wrapper">
-            <label>Nome</label>
+            <label>
+              Nome <span style={{ color: "red" }}> * </span>
+            </label>
             <input
               type="text"
               className="input-text"
@@ -65,19 +121,25 @@ const Cadastro = () => {
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
+            {errors.name && <p className="error">{errors.name}</p>}
           </div>
           <div className="input-wrapper">
-            <label>E-mail</label>
+            <label>
+              E-mail <span style={{ color: "red" }}> * </span>
+            </label>
             <input
               type="email"
               className="input-text"
-              placeholder="E-mail"
+              placeholder="email@email.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
+            {errors.email && <p className="error">{errors.email}</p>}
           </div>
           <div className="input-wrapper">
-            <label>Nascimento</label>
+            <label>
+              Nascimento <span style={{ color: "red" }}> * </span>
+            </label>
             <input
               type="date"
               className="input-text"
@@ -85,19 +147,27 @@ const Cadastro = () => {
               value={nascimento}
               onChange={(e) => setNascimento(e.target.value)}
             />
+            {errors.nascimento && <p className="error">{errors.nascimento}</p>}
           </div>
           <div className="input-wrapper" style={{ color: "white" }}>
-            <label>Telefone</label>
+            <label>
+              Telefone <span style={{ color: "red" }}> * </span>
+            </label>
             <input
               type="tel"
               className="input-text"
-              placeholder="(00) 00000 - 00"
               value={telefone}
+              placeholder="(00) 9999 9999"
               onChange={(e) => setTelefone(e.target.value)}
             />
+            {errors.telefone && <p className="error">{errors.telefone}</p>}
           </div>
           <div className="buttonDiv">
-            <button onClick={createUser} className="buttonCadastrar">
+            <button
+              onClick={createUser}
+              className={`buttonCadastrar ${isButtonDisabled ? "loading" : ""}`}
+              disabled={isButtonDisabled}
+            >
               Cadastrar
             </button>
           </div>
@@ -107,6 +177,28 @@ const Cadastro = () => {
       <div>
         <Lista data={users} deleteUser={deleteUser} />
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        toastContainerStyle={{
+          width: "100%",
+          zIndex: 9999,
+          position: "fixed",
+          top: "0",
+          left: "0",
+          margin: "0",
+          padding: "0px",
+          pointerEvents: "none",
+        }}
+      />
     </>
   );
 };
--- src/pages/lista/lista.jsx
+++ src/pages/lista/lista.jsx
@@ -1,19 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./lista.css";
+import TabPane from "antd/es/tabs/TabPane";
+import { Card, Tabs } from "antd";
 
 const Lista = ({ data, deleteUser }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  const renderUserCards = () => {
+    return data.map((user, index) => (
+      <TabPane
+        tab={<span className="custom-tab-title">{index + 1}</span>}
+        key={index}
+        className="custom-tab-pane"
+      >
+        <Card>
+          <p>NOME: {user.name}</p>
+          <p>EMAIL: {user.email}</p>
+          <p>TELEFONE: {user.telefone}</p>
+          <p>NASCIMENTO: {formatDate(user.nascimento)}</p>
+          <button onClick={() => deleteUser(user.id)}>Deletar</button>
+        </Card>
+      </TabPane>
+    ));
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div id="lista" className="lista">
-      <h1>Lista</h1>
-      <ul>
-        {data &&
-          data.map((item) => (
-            <li key={item.id}>
-              {item.nome} - {item.email} - {item.telefone} - {item.nascimento}
-              <button onClick={() => deleteUser(item.id)}>Deletar</button>
-            </li>
-          ))}
-      </ul>
+      <h1>Lista de Cadastro</h1>
+      {isMobile ? (
+        <Tabs type="card">{renderUserCards()}</Tabs>
+      ) : (
+        <div className="table-responsive">
+          <table className="lista-table table table-bordered border-primary">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>NOME</th>
+                <th>EMAIL</th>
+                <th>TELEFONE</th>
+                <th>NASCIMENTO</th>
+                <th>AÇÕES</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data &&
+                data.map((item, index) => (
+                  <tr key={item.id}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.telefone}</td>
+                    <td>{formatDate(item.nascimento)}</td>
+                    <td>
+                      <button onClick={() => deleteUser(item.id)}>
+                        Deletar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
